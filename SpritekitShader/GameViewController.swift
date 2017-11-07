13ab18401f15c21a98e29c0b6dc33d715e8bd50c
Eduard Panasiuk <ed.belarus@gmail.com>
2017-11-07 21:54:37 +0300

    Simple lightning bolt

--- SpritekitShader/GameViewController.swift
+++ SpritekitShader/GameViewController.swift
@@ -16,37 +16,27 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
+            let scene = GameScene(size: view.bounds.size)
+            scene.scaleMode = .aspectFill
             view.ignoresSiblingOrder = true
-            
             view.showsFPS = true
             view.showsNodeCount = true
+            
+            view.presentScene(scene)
         }
     }
 
     override var shouldAutorotate: Bool {
         return true
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return .landscape
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+        print("\n======\nMemory pressure!!!\n======\n")
     }
 
     override var prefersStatusBarHidden: Bool {
